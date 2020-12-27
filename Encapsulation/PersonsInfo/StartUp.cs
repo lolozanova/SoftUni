c2fcaa55cf1753cbd1dbb0346e07b093bb7bf715
Lora Lozanova <lolozanova@gmail.com>
2020-12-27 15:15:14 +0200

    Encapulacion Lab

---
+++ Encapsulation/PersonsInfo/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Person person;
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] personsInfo = Console.ReadLine().Split();
+                string firstName = personsInfo[0];
+                string secondName = personsInfo[1];
+                int age = int.Parse(personsInfo[2]);
+                decimal salary = decimal.Parse(personsInfo[3]);
+
+                 person = new Person(firstName, secondName,age ,salary);
+                persons.Add(person);
+            }
+            Team team = new Team("SoftUni");
+
+            
+
+            foreach (Person value in persons)
+            {
+               
+                team.AddPlayer(value);
+            }
+
+            Console.WriteLine(String.Join(' ', team.FirstTeam));
+            Console.WriteLine(String.Join(' ', team.ReserveTeam));
+
+
+        }
+    }
+}
